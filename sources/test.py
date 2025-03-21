1fba8a622655bcab709480d77ec4ab5be782bb7b
laaw <87073707+laawbitchh@users.noreply.github.com>
2025-03-21 09:54:22 +0100

    Update test.py

--- sources/test.py
+++ sources/test.py
@@ -4,8 +4,6 @@ import os.path
 
 FICHIER_JSON = os.path.abspath('./data/crawler.json')
 
-SEUIL_HAMMING = 18 
-
 def charger_hashes():
     """Charge la base de données des hashes depuis le fichier JSON."""
     try:
@@ -18,8 +16,8 @@ def phash_image(image):
     """Calcule le hash perceptuel (average hash) de l'image."""
     return imagehash.average_hash(image)
 
-def rechercher_image_pil(image):
-    """Recherche une image PIL dans la base en utilisant la distance de Hamming."""
+def rechercher_image_pil(image, seuil_hamming=18):
+    """Recherche une image PIL dans la base avec une distance de Hamming."""
     base_hashes = charger_hashes()
     if not base_hashes:
         print("🚨 Aucune base de données d'images trouvée.")
@@ -27,27 +25,30 @@ def rechercher_image_pil(image):
 
     new_hash = phash_image(image)
     meilleure_correspondance = None
-    distance_minimale = SEUIL_HAMMING + 1
+    distance_minimale = seuil_hamming + 1
 
     for h in base_hashes:
         hash_image = imagehash.hex_to_hash(h["hash"])
         distance = new_hash - hash_image  
 
-        print(f"🔍 Comparaison avec {h['oeuvre']} → Distance : {distance}")
+        if distance == 0:
+            print(f"✅ Correspondance exacte trouvée ! ({h['oeuvre']})")
+            return h 
 
         if distance < distance_minimale:
             distance_minimale = distance
             meilleure_correspondance = h
 
-    if meilleure_correspondance and distance_minimale <= SEUIL_HAMMING:
+    if meilleure_correspondance and distance_minimale <= seuil_hamming:
         print(f"✅ Image trouvée ! ({meilleure_correspondance['oeuvre']})")
         return meilleure_correspondance
 
     print("❌ Aucune correspondance trouvée.")
     return None
 
-def final_res(image_test):
-    resultat = rechercher_image_pil(image_test)
+
+def final_res(image_test, seuil = 18):
+    resultat = rechercher_image_pil(image_test, seuil)
     if resultat:
         print(f"🖼️ Infos de l'œuvre trouvée : {resultat}")
         return resultat['url_page']
